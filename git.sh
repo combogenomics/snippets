e8e21803ef6aeea2c5c42c1e7f13ac143ca45516
Marco Galardini <marco@ebi.ac.uk>
2014-03-06 11:07:55 +0000

    Git snippets

---
+++ git.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# A better "git log" (credits: https://coderwall.com/p/euwpig)
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
